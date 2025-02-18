58919920bff332f85f4dfecac8af0d9cbeb6b81c
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-18 11:09:51 +0700

    chore: Создать кастомного юзера

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role',)
+    list_filter = ('role',)
+    search_fields = ('username',)
+    list_per_page = 10
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    WAITER = "waiter"
+    CHEF = "chef"
+    ADMIN = "admin"
+
+    ROLE_CHOICES = [
+        (WAITER, "Официант"),
+        (CHEF, "Шеф-повар"),
+        (ADMIN, "Админ"),
+    ]
+
+    role = models.CharField(
+        max_length=10, choices=ROLE_CHOICES, default=WAITER
+    )
+
+    def __str__(self):
+        return self.username
