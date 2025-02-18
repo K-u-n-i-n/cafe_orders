da049a0b511985216cc903df9be648df1535a2ce
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-18 14:59:28 +0700

    chore: Добавить модели (Заказы и промежуточную)

--- orders/admin.py
+++ orders/admin.py
@@ -1,18 +1,42 @@
 from django.contrib import admin
 
-from .models import CustomUser, Dish
+from .models import CustomUser, Dish, Order, OrderItem
 
 
 @admin.register(CustomUser)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'role',)
+    list_display = ('id', 'username', 'email', 'role',)
     list_filter = ('role',)
     search_fields = ('username',)
+    ordering = ('username',)
     list_per_page = 10
 
 
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price',)
+    list_display = ('id', 'name', 'price',)
     search_fields = ('name',)
+    ordering = ('name',)
     list_per_page = 10
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'table_number', 'status',
+        'total_price', 'get_dishes'
+    )
+    list_filter = ('status', )
+    search_fields = ('id', 'table_number',)
+    list_per_page = 10
+
+    @admin.display(description='Блюда')
+    def get_dishes(self, obj):
+        return ', '.join([dish.name for dish in obj.dishes.all()])
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'dish', 'quantity')
+    search_fields = ('order__id', 'dish__name')
+    list_filter = ('dish',)
--- orders/models.py
+++ orders/models.py
@@ -1,16 +1,18 @@
+from decimal import Decimal
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class CustomUser(AbstractUser):
-    WAITER = "waiter"
-    CHEF = "chef"
-    ADMIN = "admin"
+    WAITER = 'waiter'
+    CHEF = 'chef'
+    ADMIN = 'admin'
 
     ROLE_CHOICES = [
-        (WAITER, "Официант"),
-        (CHEF, "Шеф-повар"),
-        (ADMIN, "Админ"),
+        (WAITER, 'Официант'),
+        (CHEF, 'Шеф-повар'),
+        (ADMIN, 'Админ'),
     ]
 
     role = models.CharField(
@@ -45,3 +47,65 @@ class Dish(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    PENDING = 'pending'
+    READY = 'ready'
+    PAID = 'paid'
+
+    ORDER_STATUS_CHOICES = [
+        (PENDING, 'В ожидании'),
+        (READY, 'Готово'),
+        (PAID, 'Оплачено'),
+    ]
+    table_number = models.IntegerField()
+    status = models.CharField(
+        max_length=10,
+        choices=ORDER_STATUS_CHOICES,
+        default=PENDING
+    )
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=Decimal('0.00')
+    )
+
+    dishes = models.ManyToManyField(
+        Dish,
+        through='OrderItem'
+    )
+
+    def recalc_total(self):
+        total = Decimal('0.00')
+        for item in self.order_items.all():
+            total += item.dish.price * item.quantity
+        self.total_price = total
+        self.save(update_fields=['total_price'])
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'Заказы'
+        ordering = ['table_number']
+        default_related_name = 'orders'
+
+    def __str__(self):
+        return f'Заказ {self.id} (Стол {self.table_number})'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE
+    )
+    dish = models.ForeignKey(
+        Dish, on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        verbose_name = 'позиция заказа'
+        verbose_name_plural = 'Позиции заказа'
+        default_related_name = 'order_items'
+
+    def __str__(self):
+        return f'{self.dish.name} x {self.quantity}'
