3b51fd4aa724eb39abb63b74f6edd6cbbe1499d4
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-20 17:17:29 +0700

    feat: Реализовать функционал входа в систему

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('orders/', include('orders.urls', namespace='orders')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('orders.urls', namespace='orders')),
 ]
