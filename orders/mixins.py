d400350919e5d41d8ec7f858d36642b63fbd9bf3
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-20 16:50:35 +0700

    fix: Исправить логику работы миксинов

--- orders/mixins.py
+++ orders/mixins.py
@@ -1,11 +1,20 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 
 
-class ChefRequiredMixin(UserPassesTestMixin):
+class ChefOrAdminRequiredMixin(UserPassesTestMixin):
     def test_func(self):
-        return self.request.user.is_chef
+        return (
+            self.request.user.is_chef or self.request.user.is_admin
+        )
 
 
-class WaiterRequiredMixin(UserPassesTestMixin):
+class WaiterOrAdminRequiredMixin(UserPassesTestMixin):
     def test_func(self):
-        return self.request.user.is_waiter
+        return (
+            self.request.user.is_waiter or self.request.user.is_admin
+        )
+
+
+class AdminRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_admin
