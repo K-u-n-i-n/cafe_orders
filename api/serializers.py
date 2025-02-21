b8b85ed22b4d0272ae6c55bedd3d20607fafedfb
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-21 21:17:12 +0700

    feat(api): Добавить проверку поля role

--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,21 @@ User = get_user_model()
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для пользовательской модели.
+
+    Поля:
+        - id: Идентификатор пользователя.
+        - username: Имя пользователя.
+        - role: Роль пользователя, выбирается из доступных вариантов.
+        - password: Пароль пользователя (только для записи).
+
+    Методы:
+        - create: Создает нового пользователя с зашифрованным паролем.
+    """
+
+    role = serializers.ChoiceField(choices=User.ROLE_CHOICES, required=True)
+
     class Meta:
         model = User
         fields = ['id', 'username', 'role', 'password']
