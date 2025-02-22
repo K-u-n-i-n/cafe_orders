2c6eec847ed9a55cc6ff91bbe17c476bc70c1c99
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-22 16:04:28 +0700

    feat(api): Реализовать эндпоинты для управления заказами

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
-from .views import AdminUserCreateAPIView
+from .views import AdminUserCreateAPIView, OrderViewSet
 
 router_v1 = DefaultRouter()
+router_v1.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='api_token_auth'),
     path('users/create/', AdminUserCreateAPIView.as_view(),
          name='admin_create_user'),
+    path('', include(router_v1.urls)),
 ]
