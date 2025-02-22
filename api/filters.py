1060edae343552c6ecd6cce2e518ff970d1e3610
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-22 16:06:23 +0700

    refactor(api): Небольшая модификация фильтра

--- api/filters.py
+++ api/filters.py
@@ -1,14 +1,16 @@
-import django_filters
+from django_filters import rest_framework as filters
 
 from orders.models import Order
 
 
-class OrderFilter(django_filters.FilterSet):
+class OrderFilter(filters.FilterSet):
     """Фильтр для модели заказа."""
 
-    table_number = django_filters.NumberFilter(field_name='table_number')
-    status = django_filters.ChoiceFilter(
-        field_name='status', choices=Order.ORDER_STATUS_CHOICES)
+    table_number = filters.NumberFilter(field_name='table_number')
+    status = filters.ChoiceFilter(
+        field_name='status',
+        choices=Order.ORDER_STATUS_CHOICES
+    ),
 
     class Meta:
         model = Order
