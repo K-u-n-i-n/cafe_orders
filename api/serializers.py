e37ea66dd44add4b9f86eaa9cf2f26a7212998ba
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-22 11:04:58 +0700

    feat(api): Написать сериализаторы для моделей блюда и заказов

--- api/serializers.py
+++ api/serializers.py
@@ -1,18 +1,14 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from orders.models import Dish, Order, OrderItem
+
 User = get_user_model()
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     """
     Сериализатор для пользовательской модели.
-
-    Поля:
-        - id: Идентификатор пользователя.
-        - username: Имя пользователя.
-        - role: Роль пользователя, выбирается из доступных вариантов.
-        - password: Пароль пользователя (только для записи).
 
     Методы:
         - create: Создает нового пользователя с зашифрованным паролем.
@@ -33,3 +29,40 @@ class CustomUserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+
+class DishSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели блюда.
+    """
+
+    class Meta:
+        model = Dish
+        fields = ['id', 'name', 'price']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели элемента заказа.
+    """
+
+    dish = DishSerializer(read_only=True)
+    dish_id = serializers.PrimaryKeyRelatedField(
+        queryset=Dish.objects.all(), source='dish', write_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'dish', 'dish_id', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели заказа.
+    """
+
+    order_items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'table_number', 'status', 'total_price', 'order_items']
+        read_only_fields = ['id', 'total_price']
