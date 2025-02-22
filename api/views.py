a43405b70607e05e041fdf0bd1483f4d2050fdc8
Alexander Kunin <K.u.n.i.n@yandex.ru>
2025-02-22 11:20:34 +0700

    feat(api): Написать вьюху для CRUD операций с заказами

--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
+from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN
+from rest_framework.viewsets import ModelViewSet
 
-from .serializers import CustomUserSerializer
+from orders.models import Order
+from .serializers import CustomUserSerializer, OrderSerializer
 
 
 class AdminUserCreateAPIView(CreateAPIView):
@@ -12,3 +17,36 @@ class AdminUserCreateAPIView(CreateAPIView):
 
     permission_classes = [IsAdminUser]
     serializer_class = CustomUserSerializer
+
+
+class OrderViewSet(ModelViewSet):
+    """
+    API для CRUD операций с заказами.
+    Поддерживается фильтрация по номеру стола и статусу.
+    """
+
+    queryset = Order.objects.all().prefetch_related('order_items__dish')
+    serializer_class = OrderSerializer
+
+    @action(detail=True, methods=['patch'], url_path='change-status')
+    def change_status(self, request, pk=None):
+        """
+        Частичное обновление статуса заказа.
+        """
+
+        order = self.get_object()
+        new_status = request.data.get('status')
+        if new_status not in dict(Order.ORDER_STATUS_CHOICES).keys():
+            return Response(
+                {'error': 'Неверный статус'},
+                status=HTTP_400_BAD_REQUEST
+            )
+        if request.user.is_chef and new_status == Order.PAID:
+            return Response(
+                {'error': 'Повар не может установить статус "Оплачено".'},
+                status=HTTP_403_FORBIDDEN
+            )
+        order.status = new_status
+        order.recalc_total()
+        order.save(update_fields=['status', 'total_price'])
+        return Response({'status': order.get_status_display()})
